3d2dcb3e5e4a8afa645c3cee6d30c54b10359371
Mark Beinker <mwb@quantlink.de>
2023-12-11 13:45:57 +0100

    add 2023 day11

---
+++ src/year2023/day11.rs
@@ -0,0 +1,66 @@
+use std::{fs::File, io};
+
+pub fn execute(part: u32, lines: io::Lines<io::BufReader<File>>) -> String {
+    match part {
+        1 => riddle_1(lines),
+        2 => riddle_2(lines),
+        _ => format!("Error: part {part} not found!"),
+    }
+}
+
+fn solve(lines: io::Lines<io::BufReader<File>>, mut expansion_factor: usize) -> String {
+    expansion_factor -= 1;
+    let mut galaxies = Vec::new();
+    let mut col_marks = Vec::new();
+    let mut row_offsets = Vec::new();
+    let mut row: usize = 0;
+    let mut row_offset = 0;
+    for l in lines {
+        let line = l.unwrap();
+        let line = line.trim();
+        if row==0 {
+            col_marks = vec![true; line.len()];
+        }
+        let mut found_galaxy = false;
+        for (col, c) in line.as_bytes().iter().enumerate() {
+            if *c==b'#' {
+                found_galaxy = true;
+                galaxies.push((row, col));
+                col_marks[col] = false;
+            }
+        }
+        if !found_galaxy {
+            row_offset += expansion_factor;
+        }
+        row_offsets.push(row_offset);
+        row += 1;
+    }
+    let mut col_offsets = Vec::new();
+    let mut col_offset: usize = 0;
+    for i in 0..col_marks.len() {
+        col_offsets.push(col_offset);
+        if col_marks[i] {
+            col_offset += expansion_factor;
+        }
+    }
+    let mut sum = 0;
+    for i in 0..galaxies.len()-1 {
+        let xi = galaxies[i].0 + row_offsets[galaxies[i].0];
+        let yi = galaxies[i].1 + col_offsets[galaxies[i].1];
+        for j in i+1..galaxies.len() {
+            let xj = galaxies[j].0 + row_offsets[galaxies[j].0];
+            let yj = galaxies[j].1 + col_offsets[galaxies[j].1];
+            sum += (xj as i64-xi as i64).abs() + (yj as i64 -yi as i64).abs();
+        }
+    }
+    format!("{sum}")
+}
+
+fn riddle_1(lines: io::Lines<io::BufReader<File>>) -> String {
+    solve(lines, 2)
+}
+
+fn riddle_2(lines: io::Lines<io::BufReader<File>>) -> String {
+    solve(lines, 1000000)
+}
+
--- src/year2023/mod.rs
+++ src/year2023/mod.rs
@@ -2,6 +2,7 @@ use std::{fs::File, io};
 
 mod day1;
 mod day10;
+mod day11;
 mod day2;
 mod day3;
 mod day4;
@@ -23,6 +24,7 @@ pub fn execute(day: u32, part: u32, lines: io::Lines<io::BufReader<File>>) -> St
         8 => day8::execute(part, lines),
         9 => day9::execute(part, lines),
         10 => day10::execute(part, lines),
+        11 => day11::execute(part, lines),
         _ => format!("Error: day {day} not found"),
     }
 }
